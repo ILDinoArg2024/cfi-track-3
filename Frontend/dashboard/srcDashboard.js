12a7baf0bdcbecc72e445ca29c3280f86696cb4a
xaviermora <xaviermoravega11@gmail.com>
2026-09-05 12:52:49 -0300

    Listado de transferencias y depósitos en frontend

--- Frontend/dashboard/srcDashboard.js
+++ Frontend/dashboard/srcDashboard.js
@@ -131,42 +131,30 @@ function ocultarMensaje(elemento) {
  * }
  */
 
-/**
- * ----------------------------------------------------------------------------
- * HISTORIAL DE MOVIMIENTOS
- * ----------------------------------------------------------------------
- * function cargarMovimientos(movimientosBD) {
- *     const contenedor = document.getElementById('listaMovimientos');
- *     
- *     movimientosBD.forEach(mov => {
- *         // Pequeña lógica para diferenciar ingresos de egresos
- *         const esIngreso = mov.monto > 0;
- *         const colorMonto = esIngreso ? 'text-blue-600' : 'text-slate-800';
- *         const signo = esIngreso ? '+' : '-';
- *         const montoFormateado = Math.abs(mov.monto).toLocaleString('es-AR', { minimumFractionDigits: 2 });
- *         
- *         const movimientoHTML = `
- *             <div class="p-4 rounded-[20px] bg-white border border-slate-200 shadow-sm flex items-center justify-between hover:bg-slate-50 transition cursor-pointer">
- *                 <div class="flex items-center gap-3">
- *                     <!-- En un caso real, los colores e íconos dependerán de la categoría del movimiento -->
- *                     <div class="w-10 h-10 rounded-full border border-slate-100 flex items-center justify-center text-slate-500 bg-slate-50">
- *                         <i class="${mov.iconoClase} text-sm"></i>
- *                     </div>
- *                     <div>
- *                         <p class="text-xs font-bold text-slate-800 flex items-center gap-1.5">
- *                             ${mov.titulo} <span class="w-1 h-1 bg-slate-400 rounded-full"></span>
- *                         </p>
- *                         <p class="text-[10px] text-slate-500 mt-0.5">${mov.descripcion}</p>
- *                     </div>
- *                 </div>
- *                 <span class="text-xs font-bold ${colorMonto} tracking-wide">${signo} $ ${montoFormateado}</span>
- *             </div>
- *         `;
- *         
- *         contenedor.innerHTML += movimientoHTML;
- *     });
- * }
- */
+import {obtenerMovimientosHTML} from "../historial/srcHistorial.js";
+const listaMovimientos = document.getElementById('listaMovimientos');
+const msgHistorialVacio = document.getElementById('msgHistorialVacio');
+
+async function renderizarMovimientos(){
+    if (!listaMovimientos || !msgHistorialVacio) return;
+
+    listaMovimientos.innerHTML = '';
+
+    let movimientosHTML = await obtenerMovimientosHTML(null, false);
+
+    if (movimientosHTML === "") {
+        // Mostrar mensaje vacío
+        msgHistorialVacio.classList.remove('hidden');
+        msgHistorialVacio.classList.add('flex');
+        listaMovimientos.classList.add('hidden');
+    } else {
+        listaMovimientos.innerHTML = movimientosHTML;
+        // Ocultar mensaje vacío
+        msgHistorialVacio.classList.add('hidden');
+        msgHistorialVacio.classList.remove('flex');
+        listaMovimientos.classList.remove('hidden');
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     
@@ -413,7 +401,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 const saldoStr = nuevoSaldo.toLocaleString('es-AR', { minimumFractionDigits: 2 });
                 const [enteros, decimales] = saldoStr.split(',');
                 saldoTotalElement.innerHTML = `$ ${enteros}<span class="text-xl opacity-80" id="saldoDecimales">,${decimales}</span>`;
-
             }, 1500);
         });
 
@@ -426,4 +413,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+
+    // Historial de movimientos
+    renderizarMovimientos();
 });
